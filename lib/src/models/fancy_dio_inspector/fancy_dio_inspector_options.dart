a82925a8619e0e20ea600e699862da494297867f
Gökhan Çavuş <gokhancavus@pm.me>
2023-06-15 13:58:16 +0300

    add `consoleOptions` parameters to `FancyDioInspectorOptions`

--- lib/src/models/fancy_dio_inspector/fancy_dio_inspector_options.dart
+++ lib/src/models/fancy_dio_inspector/fancy_dio_inspector_options.dart
@@ -1,3 +1,5 @@
+import 'package:fancy_dio_inspector/src/models/fancy_dio_inspector/fancy_dio_inspector.dart';
+
 /// This is used to configure the `FancyDioInspector` package.
 class FancyDioInspectorOptions {
   /// It controls whether to log requests or not.
@@ -12,10 +14,14 @@ class FancyDioInspectorOptions {
   /// It controls the maximum number of logs to be stored.
   final int maxLogs;
 
+  /// It controls the console logging options.
+  final FancyDioInspectorConsoleOptions consoleOptions;
+
   const FancyDioInspectorOptions({
     this.logRequests = true,
     this.logResponses = true,
     this.logErrors = true,
     this.maxLogs = 50,
+    this.consoleOptions = const FancyDioInspectorConsoleOptions(),
   });
 }
